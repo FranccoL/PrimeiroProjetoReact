6e02824260fbc553044edf721acf0eabcc1dd0b5
FranccoL <leofranco96@gmail.com>
2024-09-05 18:21:15 -0300

    Componentes compartilhados

--- firstReact/src/pages/Home.jsx
+++ firstReact/src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import Header from "../components/Header/Header"
+import Banner from "../components/Banner/Banner"
 import Footer from "../components/Footer/Footer"
 import Button from "../components/Button/Button"
+import Hero from "../components/Hero/Hero"
+import ProjectsList from "../components/ProjectsList/ProjectsList"
 
 function Home() {
     return (
         <>
         <Header/>
-        <h1>Home</h1>
-        <p>Subtitle</p>
-        <Button buttonStyle="primary" arrow>Olá</Button>
+        <div className="container">
+        <Hero />
+        <ProjectsList />
+        </div>
+        
         <Footer/>
         
         </>
